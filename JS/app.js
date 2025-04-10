2b628c37fbddfce8dd40bcd0a339466aec9d4801
hiddensmile <abdulfataimuneerohadedamola@gmail.cm>
2025-04-10 16:04:43 +0100

    second commit

--- JS/app.js
+++ JS/app.js
@@ -2,5 +2,32 @@ let modal = document.querySelector("#modal");
 let onSubmit = document.querySelector("#onSubmit");
 
 onSubmit.addEventListener("submit", function () {
-        alert("Form submitted!");
+        modal.style.display = "flex";
+        event.preventDefault();
 })
+
+closeModal.addEventListener("click", function () {
+        modal.style.display = "none";
+})
+
+function addTask() {
+        const taskInput = document.querySelector("#taskInput");
+        const taskList = document.querySelector("#taskList");
+
+        if (taskInput.value.trim() === "") {
+                alert("Please enter a task.");
+                return;
+        }
+
+        const taskItem = document.createElement("li");
+        taskList.innerHTML = `
+        ${taskInput.value}
+        <button class="delete-btn" onclick="deleteTask(this)">Delete</button>`;
+
+        taskList.appendChild(li);
+        taskInput.value = ""; // Clear the input field
+}
+
+function deleteTask(button) {
+        button.parentElement.remove();
+}
